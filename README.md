25f54921b6864b46ddd9168055d9dfd62f968994
Saif Momani <saif.aq.momani@gmail.com>
2021-04-06 12:25:46 +0300

    added two more questions

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
-# GuessingGame
+# **GuessingGame**
+
+This is a project that quizes the user about myself(Saif)
+
+- Added about me paragraph
+
+- Added the welcoming message
+
+- Added 5 questions to the guessing game
+
+- Added button to start the guessing game
+
+- Stylized the website
+
+- Added more info about the site owner
+
+- Added two more questions to the guessing game
+
+- Added the ability to see your score in the game
--- js/script.js
+++ js/script.js
@@ -1,6 +1,8 @@
 /* eslint-disable indent */
 'use strict';
 let userName = '';
+let correctAnswers = 0;
+
 // eslint-disable-next-line no-unused-vars
 function welcome() {
   userName = prompt('What is you name?');
@@ -8,6 +10,7 @@ function welcome() {
     'welcome'
   ).innerHTML = `Welcome ${userName} to the website!`;
 }
+
 // eslint-disable-next-line no-unused-vars
 function guessingGame() {
   switch (prompt('Im Software Engineering Graduate').toLowerCase()) {
@@ -15,6 +18,7 @@ function guessingGame() {
     case 'y':
       // console.log('correct');
       alert('correct');
+      correctAnswers++;
       break;
     case 'no':
     case 'n':
@@ -23,6 +27,7 @@ function guessingGame() {
       break;
   }
   //console.log('Im Software Engineering Graduate : Yes');
+
   switch (prompt('I graduated from the universty of jordan').toLowerCase()) {
     case 'yes':
     case 'y':
@@ -33,48 +38,122 @@ function guessingGame() {
     case 'n':
       // console.log('correct');
       alert('correct');
+      correctAnswers++;
       break;
   }
   //console.log('I graduated from the universty of jordan : No');
+
   switch (prompt('i worked in my field for three years').toLowerCase()) {
     case 'yes':
     case 'y':
-      // console.log('correct');
+      // console.log('incorrect');
       alert('incorrect');
       break;
     case 'no':
     case 'n':
       // console.log('correct');
       alert('correct');
+      correctAnswers++;
       break;
   }
   //console.log('i worked in my field for three years : No');
+
   switch (prompt('i like backend development').toLowerCase()) {
     case 'yes':
     case 'y':
       // console.log('correct');
       alert('correct');
+      correctAnswers++;
       break;
     case 'no':
     case 'n':
-      // console.log('correct');
+      // console.log('incorrect');
       alert('incorrect');
       break;
   }
   //console.log('i like backend development : Yes');
+
   switch (prompt('i want to someday be a AI designer').toLowerCase()) {
     case 'yes':
     case 'y':
       // console.log('correct');
       alert('correct');
+      correctAnswers++;
       break;
     case 'no':
     case 'n':
-      // console.log('correct');
+      // console.log('incorrect');
       alert('incorrect');
       break;
   }
   //console.log('i want to someday be a AI designer : Yes');
 
-  alert(`Welcome to our website ${userName}`);
+  let number = 0;
+  for (let i = 0; i < 4; i++) {
+    number = parseInt(
+      prompt(
+        `Guess my Number, its between 1 and 10, you only have ${
+          4 - i
+        } chance left`
+      )
+    );
+    if (number === 7) {
+      // console.log('correct');
+      alert('Correct!');
+      correctAnswers++;
+      break;
+    } else if (number < 7) alert('Too Low!');
+    else if (number > 7) alert('Too High!');
+    // console.log('Incorrect');
+
+    if (i === 3) alert('You ran out of chances, the answer is 7');
+  }
+  // console.log('Guess my Number : 7');
+
+  let roman = ['XI', 'XV', 'XVII', 'XX'];
+  let input = [];
+  let correct = 0;
+
+  for (let i = 0; i < 6; i++) {
+    input[i] = prompt(
+      `[I,III,IV,VI,IX,X,XI,XV,XVII,XX] Out of these 10 roman numerals which ones are larger than ten?, you have ${
+        6 - i
+      } tries`
+    ).toUpperCase();
+    for (let j = 0; j < roman.length; j++) {
+      if (input[i] === roman[j]) {
+        alert('You got one!');
+        // console.log('You got one!');
+        correct++;
+      }
+    }
+
+    let answers = '';
+
+    if (correct === roman.length) {
+      for (let i = 0; i < input.length; i++) {
+        answers += input[i] + ', ';
+      }
+      alert(
+        'You got it, Here are the correct answers[XI,XV,XVII,XX]' +
+          ` You answered [${answers}]`
+      );
+      correctAnswers++;
+      break;
+    }
+
+    if (i === 5) {
+      for (let i = 0; i < input.length; i++) {
+        answers += input[i] + ', ';
+      }
+      alert(
+        'You ran out of tries :( , Here are the correct answers[XI,XV,XVII,XX]' +
+          ` You answered [${answers}]`
+      );
+    }
+  }
+
+  alert(
+    `Welcome to our website ${userName}, you got ${correctAnswers} correct answers out of 7`
+  );
 }
